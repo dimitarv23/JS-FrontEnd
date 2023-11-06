7a71443b4649c3962087b52af913990aae73c9dd
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-06 19:08:52 +0200

    Create 02.StudentInformation.js

---
+++ 01. JS Syntax Fundamentals - Lab/02.StudentInformation.js
@@ -0,0 +1,7 @@
+function printStudentInfo (studentName, age, avgGrade) {
+    console.log(`Name: ${studentName}, Age: ${age}, Avg. Grade: ${avgGrade}`);
+}
+
+printStudentInfo('John', 15, 5.54678);
+printStudentInfo('Steve', 16, 2.1426);
+printStudentInfo('Marry', 12, 6.00);
