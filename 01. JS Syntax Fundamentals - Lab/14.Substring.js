e9f8d2382f79e00c5eac7ad3ffaaeed764e8df2e
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-06 19:10:16 +0200

    Create 14.Substring.js

---
+++ 01. JS Syntax Fundamentals - Lab/14.Substring.js
@@ -0,0 +1,8 @@
+function getSubstring(input, startIndex, endIndex) {
+    let substring = input.substring(startIndex, endIndex + 1);
+
+    console.log(substring);
+}
+
+getSubstring("ASentence", 1, 8);
+getSubstring("SkipWord", 4, 7);
