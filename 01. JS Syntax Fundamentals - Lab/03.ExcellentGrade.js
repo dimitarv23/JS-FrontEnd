4d4c4c98d95e040c553ce3efe4f28bdba4268c21
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-06 19:08:55 +0200

    Create 03.ExcellentGrade.js

---
+++ 01. JS Syntax Fundamentals - Lab/03.ExcellentGrade.js
@@ -0,0 +1,10 @@
+function isGradeExcellent (grade) {
+    if (grade >= 5.5) {
+        console.log('Excellent');
+    } else {
+        console.log('Not excellent');
+    }
+}
+
+isGradeExcellent(5.50);
+isGradeExcellent(4.35);
