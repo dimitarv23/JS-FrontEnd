0e061338f97a00e4165f77e5e6a1b16d1b477f18
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-08 18:01:32 +0200

    Create 01.Ages.js

---
+++ 02. JS Syntax Fundamentals - Exercise/01.Ages.js
@@ -0,0 +1,24 @@
+function printPersonStage(age) {
+    let output;
+
+    if (age < 0) {
+        output = "out of bounds";
+    } else if (age >= 0 && age <= 2) {
+        output = "baby";
+    } else if (age <= 13) {
+        output = "child";
+    } else if (age <= 19) {
+        output = "teenager";
+    } else if (age <= 65) {
+        output = "adult";
+    } else {
+        output = "elder";
+    }
+
+    console.log(output);
+}
+
+printPersonStage(20);
+printPersonStage(1);
+printPersonStage(100);
+printPersonStage(-1);
