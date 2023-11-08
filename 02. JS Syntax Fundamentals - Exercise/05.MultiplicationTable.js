074bc0a9876902802572b0e5e9176cd575fcb3c6
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-08 18:01:54 +0200

    Create 05.MultiplicationTable.js

---
+++ 02. JS Syntax Fundamentals - Exercise/05.MultiplicationTable.js
@@ -0,0 +1,8 @@
+function printMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+printMultiplicationTable(5);
+printMultiplicationTable(2);
