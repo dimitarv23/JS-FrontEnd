f29385fdb1c2b41e5ec02489d7325fbba9bbcbf9
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-10 10:31:26 +0200

    Create 02.ReverseArrayOfNumbers.js

---
+++ 04. JS Arrays And Strings - Lab/02.ReverseArrayOfNumbers.js
@@ -0,0 +1,9 @@
+function reverseArray(count, arr) {
+    const newArr = arr.slice(0, count).reverse();
+
+    console.log(newArr.join(" "));
+}
+
+reverseArray(3, [10, 20, 30, 40, 50]);
+reverseArray(4, [-1, 20, 99, 5]);
+reverseArray(2, [66, 43, 75, 89, 47]);
