b09567e695cf16db93397742938138bac43f1759
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-11-10 10:31:22 +0200

    Create 01.SumFirstAndLastArrayElements.js

---
+++ 04. JS Arrays And Strings - Lab/01.SumFirstAndLastArrayElements.js
@@ -0,0 +1,9 @@
+function sumFirstAndLast(arr) {
+    const result = arr[0] + arr[arr.length - 1];
+
+    console.log(result);
+}
+
+sumFirstAndLast([20, 30, 40]);
+sumFirstAndLast([10, 17, 22, 33]);
+sumFirstAndLast([11, 58, 69]);
