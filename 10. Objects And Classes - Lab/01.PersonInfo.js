d3e3877648adae99227845ca665ca487e3ea00c3
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-12-02 13:03:03 +0200

    Create 01.PersonInfo.js

---
+++ 10. Objects And Classes - Lab/01.PersonInfo.js
@@ -0,0 +1,10 @@
+function createObject(prop1, prop2, prop3) {
+    return {
+        firstName: prop1,
+        lastName: prop2,
+        age: prop3,
+    };
+}
+
+console.log(createObject("Peter", "Pan", "20"));
+console.log(createObject("George", "Smith", "18"));
